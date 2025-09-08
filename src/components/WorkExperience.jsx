c94a8d2bab35b7871896704bf66babea197f68fa
Nastia Bzova <bzovanastia@gmail.com>
2025-09-08 20:50:07 +0200

    created WorkExperience.jsx

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,20 @@
+export default function WorkExperience() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold border-b border-b pb-2 mb-4">WORK EXPERIENCE</h2>
+      <div className="space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <p className="font-semibold">Jan 2020 – Mar 2024</p>
+              <p className="text-gray-500">Job Position</p>
+            </div>
+
+            <div>
+              <p className="font-bold">COMPANY OF LOREM IPSUM</p>
+              <p className="text-gray-600 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}
