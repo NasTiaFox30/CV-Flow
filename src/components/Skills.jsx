3b27d4190228ad53bcaf8eea153c0fa5cabffc12
Nastia Bzova <bzovanastia@gmail.com>
2025-09-08 21:57:02 +0200

    creted Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,9 @@
+import SkillBar from "./SkillBar";
+
+const skillsData = [
+  { name: "Photoshop", level: 90 },
+  { name: "Illustrator", level: 75 },
+  { name: "Figma", level: 85 },
+  { name: "React", level: 60 }
+];
+
