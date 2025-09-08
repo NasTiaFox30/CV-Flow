3dcc3d6d3d73c32e4f1561649e0db5f7a3fab304
Nastia Bzova <bzovanastia@gmail.com>
2025-09-08 22:28:51 +0200

    used component <CVTemplate/>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import CVTemplate from './components/CVTemplate';
 
 export default function App() {
   return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <CVTemplate />
+    </div>
   );
 }
 
