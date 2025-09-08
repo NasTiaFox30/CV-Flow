ead67b2c877c15c7eadf6e8d02ff21a66229f3e4
Nastia Bzova <bzovanastia@gmail.com>
2025-09-08 22:07:39 +0200

    schaged style of skilbar

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -2,7 +2,7 @@ export default function SkillBar({ name, level }) {
     return (
         <div className="grid grid-cols-2 items-center py-1">
             <p>{name}</p>
-            <div className="bg-blue-500 h-2 rounded-full" style={{ width: `${level}%` }}></div>
+            <div className="bg-white h-2 rounded-full border-2 border-double border-black" style={{ width: `${level}%` }}></div>
         </div>
     );
 }
