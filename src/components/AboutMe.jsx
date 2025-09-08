b426bc3f8087faf63124aec08511e5442581f6bb
Nastia Bzova <bzovanastia@gmail.com>
2025-09-08 20:39:23 +0200

    changed format text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,7 @@
 export default function AboutMe() {
   return (
     <div className="mb-6">
-      <h2 className="text-xl font-semibold border-b pb-2 mb-2">About Me</h2>
+      <h2 className="text-xl font-bold border-b pb-2 mb-2">About Me</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
         euismod magna.
