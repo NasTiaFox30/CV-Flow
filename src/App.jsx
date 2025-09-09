9b73a9d390855543df1edab022a1f22f538c5535
Nastia Bzova <bzovanastia@gmail.com>
2025-09-09 23:18:41 +0200

    handleSelectedTemplate - for receive id template

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import CreateScreen from './components/CreateScreen';
 import HeroScreen from './components/HeroScreen';
 
 export default function App() {
+  const [templateId, setTemplateId] = useState(1);
+
+  const handleSelectedTemplate = (id) => {
+    setTemplateId(id);
+    console.log("template selected - " + id);
+  };
+
   return (
     <div className="bg-gray-100 min-h-screen p-8">
       <CVTemplate />
