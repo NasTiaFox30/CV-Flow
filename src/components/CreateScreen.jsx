51c2fa9fbaf76e84273537222fd840b6d27e922d
Nastia Bzova <bzovanastia@gmail.com>
2025-09-10 00:16:10 +0200

    update and fix CreateScreen.jsx

--- src/components/CreateScreen.jsx
+++ src/components/CreateScreen.jsx
@@ -1,17 +1,24 @@
 import CVTemplate_1 from './templates/CVTemplate_1';
-
-const templates = {
-  template1: { name: 'Classic', component: CVTemplate_1 },
-};
+// import CVTemplate_2 from './templates/CVTemplate_2';
+// import CVTemplate_3 from './templates/CVTemplate_3';
 
 export default function CreateScreen({ selectedTemplateId }) {
-    const TemplateComponent = templates[selectedTemplateId-1]?.component;
+  const templates = {
+    1: CVTemplate_1,
+    // 2: CVTemplate_2,
+    // 3: CVTemplate_3,
+  };
+  const TemplateComponent = templates[selectedTemplateId];
 
-    return (
-        <div className="flex flex-col h-screen bg-gray-200">
-            <div className="flex-1 overflow-y-auto p-8">
-                {TemplateComponent ? <TemplateComponent /> : <p className="text-center text-lg mt-12">Ops! That template not found ;(</p>}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col">
+      <div className="flex-1 overflow-y-auto p-8">
+        {TemplateComponent ? (
+          <TemplateComponent />
+        ) : (
+          <p className="text-center text-lg mt-12"> Ops! That template not found ;(</p>
+        )}
+      </div>
+    </div>
+  );
 }
