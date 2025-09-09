e45464c55617276d5bb2af700d2677d9519e94ab
Nastia Bzova <bzovanastia@gmail.com>
2025-09-09 15:11:32 +0200

    UPDATED: changing  component - SkillBar.jsx

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -1,10 +1,58 @@
 import { useState } from 'react';
 
 export default function SkillBar({ name, level }) {
-    return (
-        <div className="grid grid-cols-2 items-center py-1">
-            <p>{name}</p>
-            <div className="bg-white h-2 rounded-full border-2 border-double border-black" style={{ width: `${level}%` }}></div>
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentLevel, setCurrentLevel] = useState(level);
+
+    const handleBarClick = () => {
+        setIsEditing(true);
+    };
+    const handleInput = (event) => {
+        if (event.type === 'change') {
+            let newValue = Math.min(100, Math.max(0, event.target.value));
+            setCurrentLevel(newValue);
+        } else if (event.type === 'blur') {
+            setIsEditing(false);
+        }
+    };
+
+  return (
+    isEditing ? (
+      <div className="flex items-center justify-between py-1 relative text-gray-200">
+        <div className="relative flex-shrink-0 flex items-center w-full">
+          <div className="flex-grow bg-gray h-2 rounded-full border-2 border-double border-gray-300 mr-2">
+            <div
+              className="bg-gray-200 h-full rounded-full transition-all duration-300 ease-in-out"
+              style={{ width: `${currentLevel}%` }}
+            ></div>
+          </div>
+          <input
+            type="number"
+            value={currentLevel}
+            onChange={handleInput}
+            onBlur={handleInput}
+            autoFocus
+            className="w-16 text-center border rounded mr-2"
+          />
         </div>
-    );
+      </div>
+    ) : (
+      <div className="flex items-center justify-between py-1 relative text-gray-200">
+        <p className="flex-grow">{name}</p>
+        <div
+          className="flex-grow cursor-pointer"
+          onClick={handleBarClick}
+        >
+          <div className="flex items-center w-full">
+            <div className="flex-grow bg-gray h-2 rounded-full border-2 border-double border-gray-300 mr-2">
+              <div
+                className="bg-gray-200 h-full rounded-full transition-all duration-300 ease-in-out"
+                style={{ width: `${currentLevel}%` }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
 }
