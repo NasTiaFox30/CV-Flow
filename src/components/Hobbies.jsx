332f39bb2aeb2ba8d3b184a5572ba9847942165c
Nastia Bzova <bzovanastia@gmail.com>
2025-09-09 15:26:46 +0200

    changed itno editable fields (Hobbies.jsx)

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,12 +1,26 @@
+import EditableText from "./EditableField";
+
 export default function Hobbies() {
   const hobbiesData = [ "Graphic Design", "Photography", "Videography"];
 
   return (
     <div className="mt-8 text-sm w-full">
-      <h2 className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2">CONTACT</h2>
+      {/* <h2 className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2">HOBBIES</h2> */}
+      <EditableText
+        tag="h2"
+        className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2"
+        value="HOBBIES"
+        onUpdate={(text) => onUpdate({ ...section, content: text })}
+      />
+
       <div className="space-y-4 text-gray-200">
-        {hobbiesData.map((hobby, index) => (
-          <p key={index}>{ hobby }</p>
+        {hobbiesData.map((hobby) => (
+          <EditableText
+            tag="li"
+            className=""
+            value={hobby}
+            onUpdate={(text) => onUpdate({ ...section, content: text })}
+          />
         ))}
       </div>
     </div>
