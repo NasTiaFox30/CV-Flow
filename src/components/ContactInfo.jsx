7111dec70abdb873818c18dea2f0d60d5ce1ab8c
Nastia Bzova <bzovanastia@gmail.com>
2025-09-09 14:00:30 +0200

    changed fields (ContactIbfo.jsx)

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import EditableText from "./EditableField";
 import Icon_phone from "../assets/icons/phone.svg"
 import Icon_email from "../assets/icons/email.svg"
 import Icon_adress from "../assets/icons/adress.svg"
@@ -6,23 +6,49 @@ import Icon_adress from "../assets/icons/adress.svg"
 export default function ContactInfo() {
   return (
     <div className="mt-8 text-sm w-full">
-      <h2 className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2">Contact</h2>
+      {/* <h2 className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2">Contact</h2> */}
+      <EditableText
+        tag="h2"
+        className="text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 border-gray-200 pb-2"
+        value="Contact"
+        onUpdate={(text) => onUpdate({ ...section, content: text })}
+      />
+      
       <div className="space-y-4 text-gray-200">
         <div className="flex items-center">
           <img src={ Icon_phone }  className="h-5 w-5 mr-3" alt="" />
-          <span>+0123 4XXX 78901</span>
+          {/* <span>+0123 4XXX 78901</span> */}
+          <EditableText
+            tag="p"
+            className=""
+            value="+0123 4XXX 78901"
+            onUpdate={(text) => onUpdate({ ...section, content: text })}
+          />
+
         </div>
         <div className="flex items-center">
           <img src={ Icon_email }  className="h-5 w-5 mr-3" alt="" />
-          <span>yourname@mail.com</span>
+          {/* <span>yourname@mail.com</span> */}
+          <EditableText
+            tag="p"
+            className=""
+            value="yourname@mail.com"
+            onUpdate={(text) => onUpdate({ ...section, content: text })}
+          />
+
         </div>
         <div className="flex items-center">
           <img src={ Icon_adress }  className="h-5 w-5 mr-3" alt="" />
-          <span>
-            Your Street Address
-            <br />
-            Town/City, zip code
-          </span>
+          {/* <span>
+            Your Street Address <br />Town/City, zip code
+          </span> */}
+          <EditableText
+            tag="p"
+            className=""
+            value={"Your Street Address\nTown/City, zip code"}
+            onUpdate={(text) => onUpdate({ ...section, content: text })}
+          />
+
         </div>
       </div>
     </div>
