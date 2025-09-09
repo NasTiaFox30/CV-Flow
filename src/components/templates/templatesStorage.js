186b4435dd283c4a2c7c1e275e77210daecc2dbe
Nastia Bzova <bzovanastia@gmail.com>
2025-09-10 00:50:31 +0200

    created templateStorage

---
+++ src/components/templates/templatesStorage.js
@@ -0,0 +1,13 @@
+// imports of Templates:
+import CVTemplate_1 from './CVTemplate_1';
+
+
+export const templates = [
+  {
+    id: 1,
+    component: CVTemplate_1,
+    image: 'https://picsum.photos/seed/1/800/1000?grayscale',
+    text: 'Classic CV',
+  },
+];
+
