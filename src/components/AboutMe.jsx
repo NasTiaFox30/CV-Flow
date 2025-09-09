7c0e0ecd34f358d9b5ec963aae779b7ec37e165e
Nastia Bzova <bzovanastia@gmail.com>
2025-09-09 13:20:53 +0200

    changed struncture of fields (AboutMe.jsx)

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,11 +3,25 @@ import EditableText from "./EditableField";
 export default function AboutMe() {
   return (
     <div className="mb-6">
-      <h2 className="text-xl font-bold uppercase border-b pb-2 mb-4">ABOUT ME</h2>
-      <p>
+      {/* <h2 className="text-xl font-bold uppercase border-b pb-2 mb-4">ABOUT ME</h2> */}
+      <EditableText
+        tag="h2"
+        className="text-xl font-bold uppercase border-b pb-2 mb-4"
+        value="ABOUT ME"
+        onUpdate={(text) => onUpdate({ ...section, content: text })}
+      /> 
+
+      {/* <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
         euismod magna.
-      </p>
+      </p> */}
+      <EditableText
+        tag="p"
+        className=""
+        value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
+        euismod magna."
+        onUpdate={(text) => onUpdate({ ...section, content: text })}
+      /> 
     </div>
   );
 };
