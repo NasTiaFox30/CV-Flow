407ff06f561b347af48b2ea63a2d6151ffd0edb6
Nastia Bzova <bzovanastia@gmail.com>
2025-09-10 15:58:42 +0200

    created blockMap - mapping bloks/components of resume

---
+++ src/components/Blocks/blocksMap.js
@@ -0,0 +1,19 @@
+import AvatarFrame from "./AvatarFrame";
+import HeaderSections from "./HeaderSections";
+import ContactInfo from "./ContactInfo";
+import AboutMe from "./AboutMe";
+import Education from "./Education";
+import WorkExperience from "./WorkExperience";
+import Skills from "./Skills";
+import Hobbies from "./Hobbies";
+
+export const blocksMap = {
+  AvatarFrame,
+  HeaderSections,
+  ContactInfo,
+  AboutMe,
+  Education,
+  WorkExperience,
+  Skills,
+  Hobbies,
+};
