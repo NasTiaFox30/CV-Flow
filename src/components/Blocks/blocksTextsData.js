ad0402430e78d4b9340df5846c68a4f5593e0371
Nastia Bzova <bzovanastia@gmail.com>
2025-09-10 21:38:35 +0200

    added new blocksTestsData.js

---
+++ src/components/Blocks/blocksTextsData.js
@@ -0,0 +1,19 @@
+export const blocksTextsData = {
+  AboutMe: {
+    title: "ABOUT ME",
+    content: "Lorem ipsum dolor sit amet..."
+  },
+  Education: {
+    title: "EDUCATION",
+    items: [
+      {
+        period: "Jan 2017 – Mar 2020",
+        university: "UNIVERSITY OF LOREM IPSUM",
+        degree: "Your Degree Name Here",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+      }
+    ]
+  }
+  
+};
