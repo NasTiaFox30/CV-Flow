0fcd40207c305b4ab4d795e75246c4dcf6f2e40d
Nastia Bzova <bzovanastia@gmail.com>
2025-09-10 16:07:11 +0200

    Changes in CreateScreen.jsx

--- src/components/CreateScreen.jsx
+++ src/components/CreateScreen.jsx
@@ -1,16 +1,22 @@
 import { templateMap } from "./Templates/templatesStorage";
 
 export default function CreateScreen({ selectedTemplateId }) {
-  const TemplateComponent = templateMap[selectedTemplateId];
+  const template = templateMap[selectedTemplateId];
+
+  if (!template) {
+    return (
+      <p className="text-center text-lg mt-12">Ops! That template not found ;(</p>
+    );
+  }
+
+  const TemplateComponent = template.component;
 
   return (
-    <div className="flex flex-col">
-      <div className="flex-1 overflow-y-auto p-8">
-        {TemplateComponent ? (
-          <TemplateComponent />
-        ) : (
-          <p className="text-center text-lg mt-12"> Ops! That template not found ;(</p>
-        )}
+    <div className="flex flex-row gap-6 p-8">
+
+      {/* Selected template */}
+      <div className="flex-1 overflow-y-auto">
+        <TemplateComponent config={template.config} />
       </div>
     </div>
   );
