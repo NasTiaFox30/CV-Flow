6abcfb6b2a8456a6599241f8647a2b5c348eed75
Nastia Bzova <bzovanastia@gmail.com>
2025-09-11 02:15:28 +0200

    update Data in blocksTextsData

--- src/components/Blocks/blocksTextsData.js
+++ src/components/Blocks/blocksTextsData.js
@@ -5,15 +5,25 @@ export const blocksTextsData = {
   },
   Education: {
     title: "EDUCATION",
-    items: [
-      {
-        period: "Jan 2017 – Mar 2020",
-        university: "UNIVERSITY OF LOREM IPSUM",
-        degree: "Your Degree Name Here",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
-      }
-    ]
-  }
+    items:
+    [{
+      period: "Jan 2017 – Mar 2020",
+      degree: "Your Degree Name Here",
+      university: "UNIVERSITY OF LOREM IPSUM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"
+    }],
+    defaultItem:
+    {
+      period: "New periods",
+      degree: "New degree",
+      university: "New University",
+      description: "New description"
+    }
+  },
   
+
+
+
+
+
 };
