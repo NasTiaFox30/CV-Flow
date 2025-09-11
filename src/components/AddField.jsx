6a7ea0814a9b28933d397f0a7fc7635f2e333895
Nastia Bzova <bzovanastia@gmail.com>
2025-09-11 17:44:55 +0200

    update animation for <AddButton/>

--- src/components/AddField.jsx
+++ src/components/AddField.jsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
+
 export default function AddButton({ onClick, children = "+ Add", className = "" }) {
   return (
-    <button
+    <motion.button
       onClick={onClick}
       className={`bottom-0 mt-4 px-4 py-2 bg-gray-500 text-white rounded hover:bg-stone-600 addbutton ${className}`}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.2 }}
     >
       {children}
-    </button>
+    </motion.button>
   );
 }
