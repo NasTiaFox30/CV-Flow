f7301c60bd5c9ededca67317846f84e84a97b66d
Nastia Bzova <bzovanastia@gmail.com>
2025-10-03 15:25:34 +0200

    created blockStyle (for each custom template)

---
+++ src/components/Templates/blockStyles.js
@@ -0,0 +1,24 @@
+// Styles for each template
+export const blockStyles = {
+  template1: {
+    ContactInfo: {
+      title: "text-xl font-semibold mb-2 uppercase tracking-wide border-b-2 pb-2",
+      item: "flex items-center relative group",
+      iconButton: "h-5 w-5 mr-3 flex items-center justify-center hover:bg-gray-400 rounded transition-colors",
+    },
+    Skills: {
+      title: "text-lg font-bold border-b-2 mb-2",
+    },
+  },
+
+  template2: {
+    ContactInfo: {
+      title: "text-xl font-bold mb-4 text-stone-700",
+      item: "flex items-center gap-2",
+      iconButton: "h-6 w-6 mr-2 hover:text-stone-500",
+    },
+    Skills: {
+      title: "text-md font-semibold mb-3 text-stone-900",
+    },
+  },
+};
