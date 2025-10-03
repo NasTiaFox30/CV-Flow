155586b076ed7c9ac317b37924a71d2ad312a98e
Nastia Bzova <bzovanastia@gmail.com>
2025-10-03 22:53:47 +0200

    update aboutMe - theme

--- src/components/Blocks/AboutMe.jsx
+++ src/components/Blocks/AboutMe.jsx
@@ -1,16 +1,20 @@
 import EditableText from "../EditableField";
+import { blockStyles } from "../Templates/blockStyles";
 
-export default function AboutMe({ section, onUpdate }) {
+export default function AboutMe({ section, onUpdate, theme }) {
+  const styles = blockStyles[theme]?.AboutMe || {};
+
   return (
-    <div className="mb-6">
+    <div className="mt-8">
       <EditableText
         tag="h2"
-        className="text-xl font-bold uppercase border-b-2 pb-2 mb-4"
+        className={styles.title}
         value={section.title}
         onUpdate={(text) => onUpdate({ ...section, title: text })}
       />
       <EditableText
         tag="p"
+        className={styles.content}
         value={section.content}
         onUpdate={(text) => onUpdate({ ...section, content: text })}
       />
