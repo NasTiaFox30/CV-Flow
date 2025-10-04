fb44d0c5f59e4235daa27451968cc7ffd8405dc6
Nastia Bzova <bzovanastia@gmail.com>
2025-10-04 14:20:25 +0200

    Added button to restart template (change template)

--- src/components/CreateScreen.jsx
+++ src/components/CreateScreen.jsx
@@ -1,6 +1,8 @@
 import { templateMap } from "./Templates/templatesStorage";
+import BlocksToolbar from "./BlocksToolbar";
+import restart_icon from "../assets/icons/restart-icon.svg"
 
-export default function CreateScreen({ selectedTemplateId }) {
+export default function CreateScreen({ selectedTemplateId, onGoBack }) {
   const template = templateMap[selectedTemplateId];
 
   if (!template) {
@@ -12,12 +14,25 @@ export default function CreateScreen({ selectedTemplateId }) {
   const TemplateComponent = template.component;
 
   return (
-    <div className="flex flex-row gap-6 p-8">
+    <div className="flex flex-row h-screen">
+      
 
-      {/* Selected template */}
-      <div className="flex-1 overflow-y-auto">
+      <div className="fixed">
+        <button
+          onClick={onGoBack}
+          className="top-8 left-8 z-50 p-3 bg-white text-gray-800 rounded-full shadow-lg hover:bg-gray-200 transition-colors flex items-center space-x-2"
+          aria-label="Повернутися до вибору шаблонів"
+        >
+          <img src={ restart_icon } alt="" className="w-7 h-7"/>
+          <span>Select another template</span>
+        </button>
+        {/* <BlocksToolbar /> */}
+      </div >
+      
+      {/* Main content */}
+      <div className="flex-1 overflow-auto p-8">
         <TemplateComponent config={template.config} />
       </div>
     </div>
   );
-}
+} 
