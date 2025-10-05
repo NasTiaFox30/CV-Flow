8e3597bcb5eae5775bf502e1ed6055fa84938d7b
Nastia Bzova <bzovanastia@gmail.com>
2025-10-05 15:40:03 +0200

    added prevs to templates

--- src/components/templates/templatesStorage.js
+++ src/components/templates/templatesStorage.js
@@ -2,6 +2,10 @@
 import CVTemplate_1 from './CVTemplate_1';
 import CVTemplate_2 from './CVTemplate_2'; 
 
+// Preview imports:
+import PrevTemplate_1 from '../../assets/templates/template_1.png'
+import PrevTemplate_2 from '../../assets/templates/template_2.png'
+
 // Templates configs:
 export const templateConfig_1 = {
   left_top: ["AvatarFrame", "ContactInfo", "Skills", "Hobbies",],
@@ -22,14 +26,14 @@ export const templates = [
     id: 1,
     component: CVTemplate_1,
     config: templateConfig_1,
-    image: "https://picsum.photos/seed/1/800/1000?grayscale",
+    image: PrevTemplate_1,
     text: "Classic CV",
   },
   {
     id: 2,
     component: CVTemplate_2,
     config: templateConfig_2,
-    image: "https://picsum.photos/seed/2/800/1000?grayscale",
+    image: PrevTemplate_2,
     text: "Modern CV",
   },
 ];
