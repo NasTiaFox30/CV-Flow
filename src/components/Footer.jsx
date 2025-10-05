f37ee7a5661de397cbdebd0ae3094ee3f90dd06d
Nastia Bzova <bzovanastia@gmail.com>
2025-10-05 17:39:21 +0200

    Created Footer - copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className=" text-center text-stone-900 italic text-sm">
+        <p>Created by: Anastasiia Bzova &copy 2025</p>
+      </footer>
+    )
+}
