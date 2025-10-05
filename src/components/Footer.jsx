2cc6083b8472ef033380015d80f71bde3176965a
Nastia Bzova <bzovanastia@gmail.com>
2025-10-05 21:39:11 +0200

    update copyright sign

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className=" text-center text-stone-900 italic text-sm">
-        <p>Created by: Anastasiia Bzova &copy 2025</p>
+        <p>Created by: Anastasiia Bzova © 2025</p>
       </footer>
     )
 }
